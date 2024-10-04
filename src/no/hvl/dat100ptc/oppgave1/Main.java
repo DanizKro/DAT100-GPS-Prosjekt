0dd4ccd7bf9866e003d18bc3600326a34c9b749c
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2024-10-04 14:26:34 +0200

    ferdig del1

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		 GPSPoint point = new GPSPoint(60, 60.385390, 5.332200, 50.0);
+		 
+		 System.out.println(point.toString());
+		
+		
 		
 	}
 
